4b9d09ae464a307e63d379c537d218902b5d6331
eunn624 <eunn624@gmail.com>
2026-05-12 17:58:27 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // 제보 폼은 아래 URL로 POST 됩니다. Google Apps Script 웹앱 URL을 붙여넣으세요.
 // (배포 가이드는 README 참조)
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby1-lBJLAKrZ7jBqrEuvHPH4nKGc1Nm4VvTQ9LNAbb-426eOQdTY_1_FeKAJvmRMHFD/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7SpB1oNkcSc8y9T7O7jAko0foqXwd6gbMCBCT8gdrwtzgrxNNW68uEf3GEQNlMpd-rA/exec";
 
 window.APP_CONFIG = {
   APPS_SCRIPT_URL,
